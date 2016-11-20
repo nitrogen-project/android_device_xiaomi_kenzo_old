459dc94a67a27b68f9a038af3188367154cb9fbe
TheStrix <parthbhatia98@gmail.com>
2016-11-20 18:47:55 +0530

    kenzo: Add device specific sepolicy rules
    Change-Id: I8e9e96e2fbdfc45ec2c7321baaa1873d12652b4b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,5 +35,8 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
+# Sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
 # inherit from the proprietary version
 -include vendor/xiaomi/kenzo/BoardConfigVendor.mk
