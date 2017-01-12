37912f8515b5dd10a308b4efeb156d83c404783c
xyyx <xyyx@mail.ru>
2017-01-12 21:11:36 +0800

    androidboot.verifiedbootstate=green

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 26838785024 # 26838801408 - 16384
 
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_kenzo_defconfig
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive androidboot.verifiedbootstate=green
 
 # Properties
 TARGET_SYSTEM_PROP += device/xiaomi/kenzo/system.prop
